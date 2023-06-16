bf3dd142f0ff9bb9ad8fbc7e6d7f10eaa7592a74
DFranck <franckdufournet@hotmail.fr>
2023-06-16 10:56:08 +0700

    tp3 fini

--- JAVASCRIPT/JS TP3/script.js
+++ JAVASCRIPT/JS TP3/script.js
@@ -1,8 +1,38 @@
-// Créer un événement au scroll
+let lastScroll = 0;
+const improviseImg = document.getElementById("imgImprovise");
+const popUp = document.getElementById("popup");
+// console.log(popUp);
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+  // console.log(currentScroll);
+  if (currentScroll > lastScroll) {
+    navbar.style.height = 45 + "px";
+  } else {
+    navbar.style.height = 90 + "px";
+  }
+  lastScroll = currentScroll;
 
-// Faire apparaitre l'image de la partie improvise
+  if (currentScroll > improviseImg.offsetTop - window.innerHeight * 0.4) {
+    improviseImg.style.opacity = 1;
+    improviseImg.style.transform = "translateX(0)";
+  } else {
+    improviseImg.style.opacity = 0;
+    improviseImg.style.transform = `translateX(-200px)`;
+  }
+
+  if (currentScroll > popUp.offsetTop - window.innerHeight*0.00005) {
+    popUp.style.opacity = 1;
+    popUp.style.transform = "translateX(0px)";
+  } else {
+    popUp.style.opacity = 0;
+    popUp.style.transform = "translateX(400px)";
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popUp.style.display = "none";
+});
 
 // Faire apparaitre la popup quand on est en bas du site
 
