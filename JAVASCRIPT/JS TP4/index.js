0e384a075c425d3d2db5b54151065724d418759b
DFranck <franckdufournet@hotmail.fr>
2023-06-16 17:51:12 +0700

    fin tp4 JS mais pas réussi correctement la croix

---
+++ JAVASCRIPT/JS TP4/index.js
@@ -0,0 +1,36 @@
+// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+const btn = document.getElementById("btn");
+const sideBar = document.getElementById("side-bar");
+const btnSpan = btn.querySelectorAll("span");
+const btnSpan1 = btn.querySelector(".btn1");
+const btnSpan2 = btn.querySelector(".btn2");
+const btnSpan3 = btn.querySelector(".btn3");
+// btn.addEventListener("click", () => {
+//   console.log("HambergerClicked!");
+//     sideBar.classList.toggle("side-bar");
+//     //   sideBar.style.left = "0 !important"; POSSIBLE???????????
+// });
+
+window.addEventListener("click", (e) => {
+  //   console.log(e.target);
+  if (e.target == btn || Array.from(btnSpan).includes(e.target)) {
+    sideBar.classList.toggle("side-bar");
+    btnSpan1.style.transform = "rotate(-45deg)";
+    btnSpan2.style.transform = "rotate(45deg) translate(-10px, -10px)";
+    btnSpan3.style.display = "none";
+} else {
+    sideBar.classList.remove("side-bar");
+  }
+  btnSpan1.style.transform = "";
+  btnSpan2.style.transform = "";
+  btnSpan3.style.display = "";
+  
+});
+
+// Ranger la sidebar si on click sur le contenu principal
+
+// BONUS
+// Créer en css l'hamburger menu qui se transforme en croix
+
+// Aide
+// Priorités en CSS : id > classe > baliseHtml
