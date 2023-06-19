c72efe4944ecc506c051f2632d8aa296caa72ba5
DFranck <franckdufournet@hotmail.fr>
2023-06-19 10:27:29 +0700

    Correction JS TP6

--- JAVASCRIPT/JS TP6 Todo List/index.js
+++ JAVASCRIPT/JS TP6 Todo List/index.js
@@ -1,64 +1,65 @@
-// Créer un form pour taper les todos
-
-//Ajouter sur le dom ce qui est tapé dans l'input à la validation du form
-//mon input.value
-
-//supprimer un todo lorsque l'on click dessus (.remove())
-
-//Stocker dans le local storage la liste
-
-//Consulter le local storage au lancement de l'application pour rajouter les todos
-
 // const newToDo = document.getElementById("newToDo");
 // const toDoList = document.getElementById("toDoList");
-// const saveToDo = "";
-// let toDoSaved = window.localStorage.saveToDo;
+
+// // Consulter le local storage au lancement de l'application pour rajouter les todos
+// const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+// savedTodos.forEach((todo) => {
+//   toDoList.innerHTML += `<li>${todo}</li>`;
+// });
 
 // newToDo.addEventListener("keypress", (validNewToDo) => {
 //   if (validNewToDo.key === "Enter") {
 //     validNewToDo.preventDefault();
 //     toDoList.innerHTML += `<li>${newToDo.value}</li>`;
-//     window.localStorage.saveToDo = newToDo.value;
+//     // Stocker dans le local storage la liste
+//     savedTodos.push(newToDo.value);
+//     localStorage.setItem("todos", JSON.stringify(savedTodos));
+//     newToDo.value = "";
 //   }
 // });
 
 // toDoList.addEventListener("click", (event) => {
 //   if (event.target.tagName === "LI") {
 //     console.log("je te supprime");
+//     const index = savedTodos.indexOf(event.target.innerText);
+//     if (index > -1) {
+//       savedTodos.splice(index, 1);
+//     }
+//     localStorage.setItem("todos", JSON.stringify(savedTodos));
 //     event.target.remove();
 //   }
 // });
 
-// toDoList = toDoSaved;
-
-const newToDo = document.getElementById("newToDo");
-const toDoList = document.getElementById("toDoList");
-
-// Consulter le local storage au lancement de l'application pour rajouter les todos
-const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
-savedTodos.forEach((todo) => {
-  toDoList.innerHTML += `<li>${todo}</li>`;
-});
-
-newToDo.addEventListener("keypress", (validNewToDo) => {
-  if (validNewToDo.key === "Enter") {
-    validNewToDo.preventDefault();
-    toDoList.innerHTML += `<li>${newToDo.value}</li>`;
-    // Stocker dans le local storage la liste
-    savedTodos.push(newToDo.value);
-    localStorage.setItem("todos", JSON.stringify(savedTodos));
-    newToDo.value = "";
+//======================================================
+// Correction
+//======================================================
+//STOCK EN LOCAL
+const storeList = () => {
+  window.localStorage.toDoList = list.innerHTML;
+};
+const getList = () => {
+  if (window.localStorage.toDoList) {
+    list.innerHTML = window.localStorage.toDoList;
+  } else {
+    list.innerHTML = `<li>Clicker sur un toDo pour supprimer</li>`;
   }
+};
+window.addEventListener("load", getList());
+//AJOUTE
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  list.innerHTML += `<li>${item.value}</li>`;
+  storeList();
+  item.value = "";
 });
-
-toDoList.addEventListener("click", (event) => {
-  if (event.target.tagName === "LI") {
-    console.log("je te supprime");
-    const index = savedTodos.indexOf(event.target.innerText);
-    if (index > -1) {
-      savedTodos.splice(index, 1);
-    }
-    localStorage.setItem("todos", JSON.stringify(savedTodos));
-    event.target.remove();
+//SUPPRIME
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
   }
+  storeList();
+  //   e.target.remove();
 });
