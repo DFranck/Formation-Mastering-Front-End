91dece1107d865b777bdf0dcc88f66f56cc7cbee
DFranck <franckdufournet@hotmail.fr>
2023-06-19 20:14:05 +0700

    installation node js et continu cous api

--- JAVASCRIPT/JS Cours/Les API/index.js
+++ JAVASCRIPT/JS Cours/Les API/index.js
@@ -27,18 +27,37 @@ fetch("monlien", "objetd'option(token, autorisatien")
 //FETCH syntaxe commenté = syntaxe autre
 //==================================================
 
-fetch("data.txt")
-  .then((res) => res.text())
+fetch("data.txt").then((res) => res.text());
 //   .then((data) => console.log(data));
 
-fetch("data.json")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+fetch("data.json").then((res) => res.json());
+// .then((data) => console.log(data));
 
-// fetch("data.txt")
-//   .then((res) => {
-//     return res.text();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
+const myHeaders = new Headers();
+
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+
+// fetch("data.json", init).then((res) => console.log(res));
+
+//CRUD => Create = POST, read = GET, update = PUT, delete = DELETE
+
+const init2 = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    pseudo: "POLYMORF",
+    message: "Yo les gens!",
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+};
+document.querySelector("form").addEventListener("submit", () => {
+  fetch("http://localhost:3000/posts", init2).then(() => console.log("Data envoyé"));
+});
