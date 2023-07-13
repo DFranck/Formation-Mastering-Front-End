5a82a9b6a3848901e0bc0dc9a51a47ceb30eea97
DFranck <franckdufournet@hotmail.fr>
2023-07-13 19:56:30 +0700

    tp quiz game logic

--- JAVASCRIPT/JS TP10 Quiz App/index.js
+++ JAVASCRIPT/JS TP10 Quiz App/index.js
@@ -1,3 +1,14 @@
+class Question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
+  isCorrectAnnswer(choice) {
+    return choice === this.answer;
+  }
+}
+
 const questions = [
   new Question(
     "Quelle méthode Javascript permet de filtrer les éléments d'un tableau",
@@ -20,3 +31,48 @@ const questions = [
     "Math.round()"
   ),
 ];
+
+class Quiz {
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
+  }
+  getCurrentQuestion() {
+    return this.question[this.currentQuestionIndex];
+  }
+  guess(answer) {
+    if (this.getCurrentQuestion().isCorrectAnnswer(answer)) {
+      this.score++;
+      this.currentQuestionIndex++;
+    }
+    this.currentQuestionIndex++;
+  }
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
+}
+
+//Display Quiz
+const display = {
+  elementShown: function (id, text) {
+    let element = document.getElementById(id);
+    element.innerHTML = text;
+  },
+  question: function () {
+    this.elementShown("question");
+  },
+};
+
+//Game Logic
+quizApp = () => {
+  if (quiz.hasEnded()) {
+    //ecran de fin
+  } else {
+    //display question, choice and progress
+  }
+};
+
+//Create Quiz
+let quiz = new Quiz(questions);
+quizApp();
