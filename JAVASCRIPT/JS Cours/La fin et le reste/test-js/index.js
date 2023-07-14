e9028ecf7609544e6e78a3c7eb773f1f9587842a
DFranck <franckdufournet@hotmail.fr>
2023-07-14 16:55:37 +0700

    quelque ligne de typescript

--- JAVASCRIPT/JS Cours/La fin et le reste/test-js/index.js
+++ JAVASCRIPT/JS Cours/La fin et le reste/test-js/index.js
@@ -1,3 +1,5 @@
 import { addNum } from "./add.js";
+import { fusionArray } from "./fusionArray.js";
 
 console.log(addNum(5, 16));
+console.log(fusionArray([1, 2, 3, 4, 5], [6, 7, 8, 9]));
---
+++ JAVASCRIPT/JS Cours/La fin et le reste/test-js/index.ts
@@ -0,0 +1,8 @@
+let chiffre:42;
+
+const sayName = (name?:string)=>{
+    console.log("Bonjour" + name);
+    
+}
+
+sayName(23)
