6683441eb733a1db39e3d93d962fc547b224b4b4
DFranck <franckdufournet@hotmail.fr>
2023-07-15 11:31:17 +0700

    fin initiation typescript

---
+++ JAVASCRIPT/JS Cours/TypeScript/index.ts
@@ -0,0 +1,10 @@
+//variables
+
+let num1 = 24;
+let num2 : string
+let numOrString: number | string
+
+//Tableaux
+
+let arr= ["chien", "chat", "poisson"];
+let tableau
---
+++ JAVASCRIPT/JS Cours/TypeScript/test.js
@@ -0,0 +1,10 @@
+let array = ["chien", "chat", "poisson"];
+
+array[1] = "cheval";
+// console.log(array);
+let player = {
+  id: 0,
+  name: "Zidane",
+};
+
+console.log(player);
