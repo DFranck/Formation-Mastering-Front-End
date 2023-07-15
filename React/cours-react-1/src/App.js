5f25296304ecb3772cc843b08ee3f0cb589571ee
DFranck <franckdufournet@hotmail.fr>
2023-07-15 15:58:32 +0700

    formation react en cours

--- React/cours-react-1/src/App.js
+++ React/cours-react-1/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        //le path="*" fonctionne si jamais l'url ne correspond à rien de déclaré
+        au dessus
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ React/cours-react-1/src/pages/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A Propos</h1>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+        optio. Soluta ad temporibus suscipit, cumque vitae maxime aliquid
+        molestias adipisci laboriosam esse consectetur maiores labore hic ex
+        optio aliquam odit sunt error quod praesentium repudiandae porro
+        architecto eligendi explicabo. In ea vitae illo illum? Minus odit vel
+        placeat voluptates tempore, ut quis fuga. Similique ratione maxime
+        repellat dolorum, dolor quae culpa debitis tempora necessitatibus saepe,
+        est sunt voluptatem, hic laboriosam reprehenderit nam a explicabo ipsum
+        repellendus eaque rerum! Obcaecati iure sapiente quisquam enim at, quo
+        quas perferendis explicabo rem! Omnis, optio iure ipsum error non nisi
+        obcaecati dicta perferendis molestias! Eligendi culpa sed molestias
+        aliquid sunt dolore aut iusto itaque earum aspernatur, autem totam
+        officia illum dolores eos saepe animi voluptates accusamus quos dolorum
+        deleniti! Facilis, nemo magnam labore eaque dignissimos commodi deserunt
+        beatae animi consectetur, nulla at dolore. Magnam corrupti temporibus
+        facilis velit sed quibusdam, optio fuga, maxime ipsam debitis
+        necessitatibus ut quidem eveniet minima sint vitae! Illo cum iste
+        voluptas reiciendis magni eveniet obcaecati incidunt consequatur
+        voluptate sunt distinctio, omnis inventore atque non quis sapiente
+        aliquid pariatur labore, quam fugiat, quisquam expedita placeat!
+        Praesentium magni odit vero dicta, provident commodi blanditiis eius
+        labore corporis, quae facere, quis laudantium?
+      </p>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa qui
+        veniam, aliquam quis rerum tempora porro cupiditate expedita quidem
+        aliquid minus dolorem fugiat illo doloremque rem quae architecto optio.
+        Quidem ipsum soluta repellendus reprehenderit modi atque dolore iure,
+        quasi ea sunt nam. Dolores vel voluptates nostrum rem magni voluptas
+        doloremque quasi tempora dolorem. Itaque commodi facere velit. Tenetur
+        reprehenderit minus ab deleniti iusto voluptas itaque consequuntur in
+        rerum accusamus aperiam quasi quae quam animi vero at, eveniet,
+        perferendis mollitia sapiente aliquam. Eos, sequi. Officiis quod
+        repudiandae soluta reprehenderit repellat sit, culpa, minima incidunt
+        hic doloremque ipsam similique unde ad sapiente.
+      </p>
+    </div>
+  );
+};
+
+export default About;
+<h1>A Propos</h1>;
